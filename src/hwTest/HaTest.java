2a4295deb245c769668dff0dc01893e52784b776
hp <hp@192.168.1.5>
2019-02-19 10:23:51 -0800

    HHHH

---
+++ src/hwTest/HaTest.java
@@ -0,0 +1,27 @@
+package hwTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class HaTest {
+
+	hwTest cart = new hwTest();
+	@Test
+	public void testAddbook0() {
+		assertTrue(cart.count==0 && cart.price==0);
+	}
+	@Test
+	public void testAddbook1() {
+		cart.addbook(127);
+		assertTrue(cart.count==1 && cart.price==127);
+	}
+	@Test
+	public void testAddbook2() {
+		cart.addbook(100);
+		assertTrue(cart.count==2 && cart.price==227);
+	}
+
+}
